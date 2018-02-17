e6a5293a0f2fc8e46057158721d4916315b870eb
Matthew Bilyeu <bilyeu.matt@gmail.com>
2018-02-17 12:00:53 -0500

    Move Homebrew deps to Brewfile and use brew bundle

--- new-macos.sh
+++ new-macos.sh
@@ -14,52 +14,9 @@ which brew > /dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubuserc
 # This should also be part of startup script
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
-echo '=== Tapping ==='
+echo '=== Installing Homebrew dependencies ==='
 
-brew tap caskroom/cask
-brew tap caskroom/versions
-brew tap caskroom/fonts
-brew tap tldr-pages/tldr
-
-echo '=== Installing Casks ==='
-
-brew cask install alfred                   # https://www.alfredapp.com/
-brew cask install authy                    # https://authy.com
-brew cask install darktable                # http://www.darktable.org
-brew cask install dropbox                  # https://dropbox.com
-brew cask install firefox                  # https://www.mozilla.org/en-US/firefox/
-brew cask install flux                     # https://justgetflux.com/
-brew cask install font-fira-code           # https://github.com/tonsky/FiraCode
-brew cask install iterm2-nightly           # http://iterm2.com/
-brew cask install java                     # https://java.com/en/download/
-brew cask install keepingyouawake          # https://github.com/newmarcel/KeepingYouAwake
-brew cask install licecap                  # http://www.cockos.com/licecap/
-brew cask install middleclick              # http://rouge41.com/labs/
-brew cask install private-internet-access  # https://www.privateinternetaccess.com
-brew cask install spectacle                # https://www.spectacleapp.com/
-brew cask install transmission             # https://transmissionbt.com
-
-echo '=== Installing Brews ==='
-
-brew install aspell                        # http://aspell.net/
-brew install bash-completion               # https://github.com/scop/bash-completion
-brew install bash-git-prompt               # https://github.com/magicmonty/bash-git-prompt
-brew install fd                            # https://github.com/sharkdp/fd
-brew install git                           # https://git-scm.com/
-brew install gnupg2                        # https://www.gnupg.org/
-brew install htop                          # http://hisham.hm/htop/
-brew install jq                            # https://stedolan.github.io/jq/
-brew install leiningen                     # http://leiningen.org/
-brew install markdown                      # http://daringfireball.net/projects/markdown/
-brew install pandoc                        # http://pandoc.org/
-brew install python3                       # https://www.python.org
-brew install shellcheck                    # https://www.shellcheck.net/
-brew install speedtest-cli                 # https://github.com/sivel/speedtest-cli
-brew install the_silver_searcher           # https://github.com/ggreer/the_silver_searcher/
-brew install tldr                          # http://tldr-pages.github.io/
-brew install trash                         # http://hasseg.org/trash/
-brew install tree                          # http://mama.indstate.edu/users/ice/tree/
-brew install wget                          # https://www.gnu.org/software/wget/
+brew bundle
 
 echo '=== Configuring git ==='
 
@@ -71,11 +28,7 @@ git config --global push.default simple
 echo '=== Installing Spacemacs ==='
 
 # https://github.com/syl20bnr/spacemacs
-
-brew tap d12frosted/emacs-plus
-brew unlink emacs-plus
-brew install emacs-plus
-brew linkapps emacs-plus
+# emacs-plus installed via Homebrew above
 
 [ -f ~/.emacs.d ] && git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 [ -f ~/.dotfiles ] && git clone git@github.com:bi1yeu/dotfiles.git ~/.dotfiles && ./.dotfiles/link-dotfiles.sh
